41b8bc4af963aad37018dfe1f16f69776e360569
daud gul <codrdaud@gmail.com>
2023-09-10 14:29:59 +0530

    added node modules

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
+
 const dotenv = require('dotenv')
 
 dotenv.config({path: './config.env'})
 
 const DB = process.env.DATABASE;
 
-mongoose.connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-}).then(con => {
-    console.log(con)
-    console.log('DB connection successful!')
-}).catch(error => {
-    // Handle the error
-    console.log("I am Error" , error)
-  });
+
+
+
+// mongoose.connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+// }).then(con => {
+//     console.log(con)
+//     console.log('DB connection successful!')
+// }).catch(error => {
+//     // Handle the error
+//     console.log("I am Error" , error)
+//   });
 
 const app = require('./app')
 
