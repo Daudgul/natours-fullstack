2cd39b3f14f24180669d109e889bd3180dceb392
daud gul <codrdaud@gmail.com>
2023-09-11 13:21:41 +0530

    added mongo db

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const dotenv = require('dotenv')
 
 dotenv.config({path: './config.env'})
 
 const DB = process.env.DATABASE;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(DB, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 
 
