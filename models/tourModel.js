9c2f60b0c24a3985ef2460f225d5c5fb091695e8
daud gul <codrdaud@gmail.com>
2023-09-21 08:54:59 +0530

    added document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 
 const tourSchema = new mongoose.Schema({
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema({
       required: [true, 'A tour must have a name'],
       unique: true
     },
+    slug: String,
     duration:{
         type: Number,
         required: ['A tour must have a duration']
@@ -50,7 +52,33 @@ const tourSchema = new mongoose.Schema({
     },
     startDates: [Date],
     description: String
+  },{
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  });
+
+  tourSchema.virtual('durationWeeks').get(function() {
+    return this.duration / 7;
+  });
+
+  
+//  DOCUMENT MIDDLEWARE: runs befor .save() and .create()
+  tourSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
   })
+
+  // Can use multiple middleware in this 
+  // tourSchema.pre('save', function(next) {
+  //   console.log('Will save document...')
+  //   next();
+  // })
+
+  // THis is after call the middleware we use post
+  // tourSchema.post('save', function(doc, next) {
+  //   console.log(doc)
+  //   next();
+  // })
 
   const Tour = mongoose.model('Tour', tourSchema)
 
