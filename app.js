d05cd860710b50b4773e4cb73ab57c09afa12628
Daud Gul <codrdaud@gmail.com>
2023-09-24 14:27:24 +0530

    removed Code

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.use(express.static(`${__dirname}/public`))
 
 
 
-// app.use((req,res,next) => {
-//     console.log('Hello from the middleware')
-//     next();
-// })
+app.use((req,res,next) => {
+    console.log('Hello from the middleware')
+    next();
+})
 
 app.use((req,res,next) => {
     req.requestTime = new Date().toISOString();
