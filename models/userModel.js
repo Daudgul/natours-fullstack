31ee8c137d639d5b8d49a33dfa3d524fc671dbf0
Daud Gul <codrdaud@gmail.com>
2023-09-30 22:39:25 +0530

    implemented the password dcript password

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator'); // You need to import the validator library if you're using it
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -43,6 +44,17 @@ const userSchema = new mongoose.Schema({
         },
     },
 });
+
+
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) return next();
+
+    this.password = await bcrypt.hash(this.password, 12);
+
+    this.passwordConfirm = undefined;
+    next();
+})
+
 
 const User = mongoose.model('User', userSchema);
 
